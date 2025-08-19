4eb18efa4ab6f96b54b24a56ae131dcfce2da562
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-08-19 12:30:00 +0700

    feat: updated the header bar
    * Updated the styling
    * Added dynamic time

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import './App.css'
-import Counter from './components/Counter'
 import Header from './components/Header/Header'
-import Hello from './components/Hello'
 
 function App() {
   return (
     <>
       <Header />
 
-      <div className='bg-blue-500'>
-        <Hello />
-
-        <Counter />
-      </div>
+      <main></main>
     </>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,24 @@
+function getCurrentTime(): string {
+  const now = new Date()
+  return new Intl.DateTimeFormat('en-US', { day: '2-digit', month: 'short' }).format(now)
+}
+
 export default function Header() {
   return (
-    <header className='flex w-full items-center justify-between gap-2 rounded-md p-5'>
-      <div className='flex items-center gap-5'>
-        <h1 className='text-2xl font-bold'>My Todo List</h1>
-        <span>(10 due today)</span>
+    <header className='flex min-h-[10svh] w-full items-center justify-between gap-2 rounded-md p-5'>
+      <div className='flex h-full items-center gap-3'>
+        <h1 className='text-2xl font-bold'>Tasks</h1>
+        <span className='text-2xl'>{getCurrentTime()}</span>
       </div>
 
-      <nav className='flex gap-5'>
-        <ul className='rounded-md bg-blue-500 px-5 py-2 font-bold text-stone-50'>Day</ul>
-        <ul className='rounded-md bg-blue-500 px-5 py-2 font-bold text-stone-50'>Week</ul>
-        <ul className='rounded-md bg-blue-500 px-5 py-2 font-bold text-stone-50'>Month</ul>
-        <ul className='rounded-md bg-blue-500 px-5 py-2 font-bold text-stone-50'>All</ul>
-      </nav>
+      <input className='h-full min-w-[45%] rounded-md bg-stone-200 p-2' type='search' placeholder='Search todos' />
 
-      <input type='search' placeholder='Search todos'></input>
+      <nav className='flex cursor-pointer rounded-lg bg-blue-500 font-bold text-stone-50'>
+        <ul className='rounded-s-md px-5 py-2 hover:bg-blue-700'>Day</ul>
+        <ul className='px-5 py-2 hover:bg-blue-700'>Week</ul>
+        <ul className='px-5 py-2 hover:bg-blue-700'>Month</ul>
+        <ul className='rounded-e-md px-5 py-2 hover:bg-blue-700'>All</ul>
+      </nav>
     </header>
   )
 }
