d09878cad6cb7935c2819d19a28b3ef685de31e8
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-08-21 15:05:07 +0700

    feat: updated the Counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,3 +1,52 @@
+import { useState } from 'react'
+
 export default function Counter() {
-  return <div className='text-2xl font-bold'>Counter: 15</div>
+  const [state, setState] = useState({
+    count: 0,
+    user: 'Hallo'
+  })
+
+  return (
+    <>
+      <div className='text-2xl font-bold'>
+        Counter: {state.count} {state.user}
+      </div>
+
+      <button
+        className='mr-5 border p-2'
+        onClick={() => {
+          setState((state) => ({
+            ...state,
+            count: state.count - 1
+          }))
+        }}
+      >
+        Decrease
+      </button>
+
+      <button
+        className='mr-5 border p-2'
+        onClick={() => {
+          setState((state) => ({
+            ...state,
+            count: state.count + 1
+          }))
+        }}
+      >
+        Increase
+      </button>
+
+      <button
+        className='border p-2'
+        onClick={() => {
+          setState((state) => ({
+            ...state,
+            user: 'Spell'
+          }))
+        }}
+      >
+        Change User
+      </button>
+    </>
+  )
 }
