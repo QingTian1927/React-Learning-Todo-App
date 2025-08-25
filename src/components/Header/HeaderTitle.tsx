9aa5ae37524aac569cd06f94ef3909c9a071e329
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-08-25 15:16:21 +0700

    refactor: minor text color change

--- src/components/Header/HeaderTitle.tsx
+++ src/components/Header/HeaderTitle.tsx
@@ -4,7 +4,7 @@ type HeaderTitleProps = {
 
 export default function HeaderTitle({ time }: HeaderTitleProps) {
   return (
-    <div className='text-pastel-turquoise flex h-full items-center gap-3'>
+    <div className='text-pastel-gray-dark flex h-full items-center gap-3'>
       <h1 className='text-2xl font-bold'>Tasks</h1>
       <span className='text-2xl'>{time}</span>
     </div>
