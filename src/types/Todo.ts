e36c8232e63c5d21d00c134d49b41649d1e17e76
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-08-25 10:11:00 +0700

    feat: added a helper method to create empty Todo
    * Also refactored some code for conciseness and better reusability.

--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,4 +1,5 @@
-export type Priority = 'Low' | 'Medium' | 'High' | 'Critical'
+export const priorities = ['Low', 'Medium', 'High', 'Critical'] as const
+export type Priority = (typeof priorities)[number]
 
 export const priorityOrder: Record<Priority, number> = {
   Low: 1,
@@ -13,9 +14,21 @@ export type Todo = {
   description: string
   dueDate: Date
   priority: Priority
-  status: 'Not Started' | 'In Progress' | 'Completed'
+  status: Status
   tags: string[]
 }
 
 export const statuses = ['Not Started', 'In Progress', 'Completed'] as const
 export type Status = (typeof statuses)[number]
+
+export function createEmptyTodo(): Todo {
+  return {
+    id: '',
+    title: '',
+    description: '',
+    dueDate: new Date(),
+    priority: 'Low',
+    status: 'Not Started',
+    tags: ['']
+  }
+}
