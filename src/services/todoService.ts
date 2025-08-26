5245a2fa34a1188afced0a3263d987a6da9b2ae3
QingTian1927 <120935290+QingTian1927@users.noreply.github.com>
2025-08-26 11:01:34 +0700

    feat: updated the todo service to accommodate offline actions

--- src/services/todoService.ts
+++ src/services/todoService.ts
@@ -1,5 +1,7 @@
+import { createOfflineAction } from '../types/OfflineAction'
 import type { Priority, Status, Todo } from '../types/Todo'
 import { cacheService } from './cacheService'
+import { offlineService } from './offlineService'
 
 const BASE_URL = 'https://shrimo.com/fake-api/todos'
 
@@ -18,63 +20,113 @@ type TodoResponse = {
   tags: string[]
 }
 
-async function getTodos(): Promise<Todo[]> {
-  const response = await fetch(`${BASE_URL}`)
+async function getTodos(isOnline = navigator.onLine): Promise<Todo[]> {
+  console.log({ isOnline })
 
-  if (!response.ok) {
-    throw new Error(`Failed to fetch todos: ${response.statusText}`)
+  if (isOnline) {
+    const response = await fetch(`${BASE_URL}`)
+    if (!response.ok) {
+      throw new Error(`Failed to fetch todos: ${response.statusText}`)
+    }
+
+    const rawData: TodoResponse[] = await response.json()
+    const todos: Todo[] = rawData.map((item) => ({
+      ...item,
+      id: item._id,
+      dueDate: new Date(item.dueDate)
+    }))
+
+    cacheService.save(todos)
+    return todos
   }
 
-  const rawData: TodoResponse[] = await response.json()
-  const todos: Todo[] = rawData.map((item) => ({
-    ...item,
-    id: item._id,
-    dueDate: new Date(item.dueDate)
-  }))
-
-  cacheService.save(todos)
-  return todos
+  const cached = cacheService.load()
+  if (Array.isArray(cached)) {
+    return cached.map((todo) => ({
+      ...todo,
+      dueDate: todo.dueDate ? new Date(todo.dueDate) : undefined
+    }))
+  }
+  return []
 }
 
-async function createTodo(todo: Omit<Todo, 'id'>): Promise<Todo> {
-  const response = await fetch(`${BASE_URL}`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(todo)
-  })
+async function createTodo(todo: Omit<Todo, 'id'>, isOnline = navigator.onLine): Promise<Todo> {
+  if (isOnline) {
+    const response = await fetch(`${BASE_URL}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(todo)
+    })
 
-  if (!response.ok) {
-    throw new Error(`Failed to create todo: ${response.statusText}`)
+    if (!response.ok) {
+      throw new Error(`Failed to create todo: ${response.statusText}`)
+    }
+
+    const result: ApiResponse<TodoResponse> = await response.json()
+    const newTodo = { ...result.data, dueDate: new Date(result.data.dueDate), id: result.data._id }
+    return newTodo
   }
 
-  const result: ApiResponse<TodoResponse> = await response.json()
-  const newTodo = { ...result.data, dueDate: new Date(result.data.dueDate), id: result.data._id }
-  return newTodo
+  const offlineAction = createOfflineAction({ ...todo, id: '' }, 'create')
+  offlineService.enqueue(offlineAction)
+
+  const cached: Todo[] = (cacheService.load() as Todo[]) ?? []
+  const newLocalTodo = { ...todo, id: offlineAction.id }
+  cacheService.save([newLocalTodo, ...cached])
+
+  return newLocalTodo
 }
 
-async function deleteTodo(id: string) {
-  const response = await fetch(`${BASE_URL}/${id}`, {
-    method: 'DELETE'
-  })
+async function deleteTodo(id: string, isOnline = navigator.onLine) {
+  if (isOnline) {
+    const response = await fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE'
+    })
 
-  if (!response.ok) {
-    throw new Error(`Failed to delete todo: ${response.statusText}`)
+    if (!response.ok) {
+      throw new Error(`Failed to delete todo: ${response.statusText}`)
+    }
   }
+
+  const offlineAction = createOfflineAction({ id } as Todo, 'delete')
+  offlineService.enqueue(offlineAction)
+
+  const cached: Todo[] = (cacheService.load() as Todo[]) ?? []
+  const updatedCache = cached.filter((todo) => todo.id !== id)
+  cacheService.save(updatedCache)
+
+  return
 }
 
-async function updateTodo(id: string, todo: Partial<Omit<Todo, 'id'>>): Promise<Todo> {
-  const response = await fetch(`${BASE_URL}/${id}`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(todo)
-  })
+async function updateTodo(id: string, todo: Partial<Omit<Todo, 'id'>>, isOnline = navigator.onLine): Promise<Todo> {
+  if (isOnline) {
+    const response = await fetch(`${BASE_URL}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(todo)
+    })
 
-  if (!response.ok) {
-    throw new Error(`Failed to update: todo: ${response.statusText}`)
+    if (!response.ok) {
+      throw new Error(`Failed to update: todo: ${response.statusText}`)
+    }
+    const result: ApiResponse<TodoResponse> = await response.json()
+    const updatedTodo = { ...result.data, dueDate: new Date(result.data.dueDate), id: result.data._id }
+    return updatedTodo
   }
-  const result: ApiResponse<TodoResponse> = await response.json()
-  const updatedTodo = { ...result.data, dueDate: new Date(result.data.dueDate), id: result.data._id }
-  return updatedTodo
+
+  const offlineAction = createOfflineAction({ ...todo, id } as Todo, 'update')
+  offlineService.enqueue(offlineAction)
+
+  const cached: Todo[] = (cacheService.load() as Todo[]) ?? []
+  const updatedCache = cached.map((oldTodo) => (oldTodo.id === id ? { ...oldTodo, ...todo } : oldTodo))
+  cacheService.save(updatedCache)
+
+  const updatedTodo = updatedCache.find((t) => t.id === id)
+  if (!updateTodo) {
+    throw new Error(`Todo with id {id} was not found in cache`)
+  }
+
+  return updatedTodo as Todo
 }
 
 export const todoService = {
